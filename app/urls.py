da66a83f073bdaa8951a00076f75228547384770
Abolarin Mutiu <kayodemutiu12@gmail.com>
2024-06-17 14:16:53 +0100

    Initial commit

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
+    path('schema', SpectacularAPIView.as_view(), name='schema'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema')),
     path('admin/', admin.site.urls),
     path('auth/', include('users.api.urls')),
 ]
