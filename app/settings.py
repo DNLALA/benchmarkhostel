21ffeaf9782afb8eb8f46b549423a1823a8d47ab
Abolarin Mutiu <kayodemutiu12@gmail.com>
2024-06-17 16:34:41 +0100

    Initial commit

--- app/settings.py
+++ app/settings.py
@@ -108,9 +108,22 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # 'myapp.authentication.CustomAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'benchmarkhostel Documentations'
+}
+
 
 LANGUAGE_CODE = 'en-us'
 
