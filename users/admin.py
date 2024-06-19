509b75e29e661a39dc71b25bcb4b60a2b332491e
Abolarin Mutiu <kayodemutiu12@gmail.com>
2024-06-19 14:54:54 +0100

    19-jun

--- users/admin.py
+++ users/admin.py
@@ -10,7 +10,7 @@ class UserAdmin(BaseUserAdmin):
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         (_('Personal info'), {'fields': ('reg_no',)}),
-        (_('Permissions'), {'fields': ('is_staff', 'is_active', 'is_student', 'is_warden')}),
+        (_('Permissions'), {'fields': ('is_staff', 'is_active', 'is_student', 'is_warden', 'has_hostel')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
