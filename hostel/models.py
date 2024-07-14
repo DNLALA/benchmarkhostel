e07d467ccf10c09851a5f1d09996d1e041bd0f89
Abolarin Mutiu <kayodemutiu12@gmail.com>
2024-07-14 17:24:30 +0100

    14-jul

--- hostel/models.py
+++ hostel/models.py
@@ -2,12 +2,16 @@ from django.db import models
 from users.models import User
 
 class Hostel(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='hostel')
-    room_type = models.CharField(max_length=100)
-    seater = models.CharField(max_length=100)
-    hostel = models.CharField(max_length=255)
-    room_number = models.CharField(max_length=50)
-    bed = models.CharField(max_length=50)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='hostel', blank=True, null=True)
+    room_type = models.CharField(max_length=100, blank=True, null=True)
+    seater = models.CharField(max_length=100, blank=True, null=True)
+    hostel = models.CharField(max_length=255, blank=True, null=True)
+    issues = models.CharField(max_length=255, blank=True, null=True)
+    tranfar = models.BooleanField(default=False)
+    maintenance = models.CharField(max_length=255, blank=True, null=True)
+    reports = models.CharField(max_length=255, blank=True, null=True)
+    room_number = models.CharField(max_length=50, blank=True, null=True)
+    bed = models.CharField(max_length=50, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
